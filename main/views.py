2097e956fa0e2af9fc62441aecd83bb07e90a658
izzydharma <147742855+izzydharma@users.noreply.github.com.>
2024-09-09 13:14:19 +0700

    fix

--- main/views.py
+++ main/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'npm' : '2306256425',
+        'name': 'Made Izzy Prema Dharma',
+        'class': 'KKI'
     }
 
     return render(request, "main.html", context)    
-    "add"
+    
