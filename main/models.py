03168de471e57ea613c7e2f2d6c766d46fc56410
izzydharma <147742855+izzydharma@users.noreply.github.com.>
2024-09-12 01:15:09 +0700

    tutorial 2, sorry late my work i did at campus is in the other rep. you can see in the scele for tutorial 0 labeled new

--- main/models.py
+++ main/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+import uuid
 
 class MoodEntry(models.Model):
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
     mood_intensity = models.IntegerField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
 
     @property
     def is_mood_strong(self):
