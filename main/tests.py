d7e2684d2cf6faf5a1b646147f647234da8e8022
izzydharma <147742855+izzydharma@users.noreply.github.com.>
2024-09-19 12:37:46 +0700

    question 4

--- main/tests.py
+++ main/tests.py
@@ -3,24 +3,7 @@ from django.utils import timezone
 from .models import MoodEntry
 
 class MainTest(TestCase):
-    def test_main_url_is_exist(self):
-        response = Client().get('')
-        self.assertEqual(response.status_code, 200)
-
-    def test_main_using_main_template(self):
-        response = Client().get('')
-        self.assertTemplateUsed(response, 'main.html')
-
-    def test_nonexistent_page(self):
-        response = Client().get('/skibidi/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_strong_mood_user(self):
-        now = timezone.now()
-        mood = MoodEntry.objects.create(
-          mood="Happy",
-          time = now,
-          feelings = "I'm happy, even though my clothes are soaked from the rain :(",
-          mood_intensity = 8,
-        )
-        self.assertTrue(mood.is_mood_strong)
+    def test_main_template_uses_correct_page_title(self):
+        response = Client().get("/")
+        html_response = response. content.decode( "utf8")
+        self.assertIn("PBD Mental Health Tracker", html_response)
