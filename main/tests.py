b4271a21eadb5d87870fb2545f4c2d14ade9cb45
izzydharma <147742855+izzydharma@users.noreply.github.com.>
2024-09-20 21:10:23 +0700

    fix test.py

--- main/tests.py
+++ main/tests.py
@@ -1,9 +1,24 @@
 from django.test import TestCase, Client
 from django.utils import timezone
 from .models import MoodEntry
+from django.contrib.auth.models import User
+
 
 class MainTest(TestCase):
+    def setUp(self):
+        # Create a test user
+        self.user = User.objects.create_user(username='izzydharma', password='kamuaku40')
+
     def test_main_template_uses_correct_page_title(self):
-        response = Client().get("/")
-        html_response = response. content.decode( "utf8")
+        # Log in the client
+        self.client.login(username='izzydharma', password='kamuaku40')
+
+        self.client.cookies['last_login'] = '2024-09-20 10:00:00'
+
+
+        # Now get the response
+        response = self.client.get("/")
+        html_response = response.content.decode("utf8")
+
+        # Check if the title is present
         self.assertIn("PBD Mental Health Tracker", html_response)
